2ed4beccab62a4cf7a63926e110f7ed111a246f1
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-06-28 19:59:52 +0500

    publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     bot = telegram.Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
-    bot.send_message(text='Hi there', chat_id='@cool_space_photos')
+    bot.send_photo(chat_id='@cool_space_photos', photo=open('images/nasa_epic_00.png', 'rb'))
 
 
 if __name__ == '__main__':
