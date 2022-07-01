b1529764ed26cae5a73bb05ed78455b1110d64de
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-01 18:11:12 +0500

    Define function for later reusing

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -3,17 +3,19 @@ import argparse
 from file_utils import download_image
 
 
-def main(launch_id='latest'):
-    parser = argparse.ArgumentParser(description='Загружает изображения запуска SpaceX')
-    parser.add_argument('--id', help='ID запуска', default='latest')
-    args = parser.parse_args()
-
-    url = f'https://api.spacexdata.com/v5/launches/{args.id}'
+def fetch_spacex_images(launch_id='latest'):
+    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
     response = requests.get(url)
     response.raise_for_status()
     for n, img in enumerate(response.json()['links']['flickr']['original']):
         download_image(img, f'spacex_{n:02d}.jpg')
 
+
+def main():
+    parser = argparse.ArgumentParser(description='Загружает изображения запуска SpaceX')
+    parser.add_argument('--id', help='ID запуска', default='latest')
+    args = parser.parse_args()
+    fetch_spacex_images(args.id)
 
 if __name__ == '__main__':
     main()
