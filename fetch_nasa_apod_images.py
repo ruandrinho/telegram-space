9c52651de622bec56f461e652a4723ae67105c2c
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-01 18:18:10 +0500

    Clean the code

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -6,14 +6,15 @@ from file_utils import download_image, get_file_extension
 from dotenv import load_dotenv
 
 
-def fetch_nasa_apod_images(days=1):
+def fetch_nasa_apod_images(api_key, days=1):
     url = 'https://api.nasa.gov/planetary/apod'
     today = datetime.date.today()
-    response = requests.get(url, params={
-        'api_key': os.getenv('NASA_API_KEY'),
+    params = {
+        'api_key': api_key,
         'end_date': today,
         'start_date': today - datetime.timedelta(days=days-1)
-    })
+    }
+    response = requests.get(url, params=params)
     for n, item in enumerate(response.json()):
         if item['media_type'] != 'image':
             continue
@@ -26,9 +27,7 @@ def main():
     parser = argparse.ArgumentParser(description='Загружает изображения NASA APOD')
     parser.add_argument('-d', '--days', type=int, help='Количество дней', default=1)
     args = parser.parse_args()
-    fetch_nasa_apod_images(args.days)
-
-
+    fetch_nasa_apod_images(os.getenv('NASA_API_KEY'), args.days)
 
 if __name__ == '__main__':
     main()
