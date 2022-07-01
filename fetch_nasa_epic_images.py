e35edb63d22181b4ae142129e520918497ed5784
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-01 18:14:23 +0500

    Define a function for later reusing

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -6,17 +6,12 @@ from file_utils import download_image, get_file_extension
 from dotenv import load_dotenv
 
 
-def main():
-    load_dotenv()
-    parser = argparse.ArgumentParser(description='Загружает изображения NASA EPIC')
-    parser.add_argument('-c', '--count', type=int, help='Количество', default=1)
-    args = parser.parse_args()
-
+def fetch_nasa_epic_images(count=1):
     url = 'https://api.nasa.gov/EPIC/api/natural'
     response = requests.get(url, params={
         'api_key': os.getenv('NASA_API_KEY')
     })
-    for n, item in enumerate(response.json()[:args.count]):
+    for n, item in enumerate(response.json()[:count]):
         img_datetime = datetime.datetime.fromisoformat(item['date'])
         img_url = f'https://api.nasa.gov/EPIC/archive/natural/{img_datetime.year}/{img_datetime.month:02d}/{img_datetime.day:02d}/png/{item["image"]}.png'
         download_image(
@@ -24,6 +19,14 @@ def main():
             f'nasa_epic_{n:02d}.png', 
             {'api_key': os.getenv('NASA_API_KEY')}
         )
+
+
+def main():
+    load_dotenv()
+    parser = argparse.ArgumentParser(description='Загружает изображения NASA EPIC')
+    parser.add_argument('-c', '--count', type=int, help='Количество', default=1)
+    args = parser.parse_args()
+    fetch_nasa_epic_images(args.count)
 
 
 if __name__ == '__main__':
