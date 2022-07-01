90e3442728850b81d409f7d4e9d530534dbc49a2
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-01 18:19:37 +0500

    Clean the code

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -6,11 +6,9 @@ from file_utils import download_image, get_file_extension
 from dotenv import load_dotenv
 
 
-def fetch_nasa_epic_images(count=1):
+def fetch_nasa_epic_images(api_key, count=1):
     url = 'https://api.nasa.gov/EPIC/api/natural'
-    response = requests.get(url, params={
-        'api_key': os.getenv('NASA_API_KEY')
-    })
+    response = requests.get(url, params={'api_key': api_key})
     for n, item in enumerate(response.json()[:count]):
         img_datetime = datetime.datetime.fromisoformat(item['date'])
         img_url = f'https://api.nasa.gov/EPIC/archive/natural/{img_datetime.year}/{img_datetime.month:02d}/{img_datetime.day:02d}/png/{item["image"]}.png'
@@ -26,7 +24,7 @@ def main():
     parser = argparse.ArgumentParser(description='Загружает изображения NASA EPIC')
     parser.add_argument('-c', '--count', type=int, help='Количество', default=1)
     args = parser.parse_args()
-    fetch_nasa_epic_images(args.count)
+    fetch_nasa_epic_images(os.getenv('NASA_API_KEY'), args.count)
 
 
 if __name__ == '__main__':
